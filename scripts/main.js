62f253deee80748ccee05c99c7b77f5311928f54
Steven Nguyen <stevennguyen223@gmail.com>
2026-03-05 22:33:04 -0500

    nvm now it's fixed

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,8 @@ const image = document.querySelector("img");
 image.addEventListener("click", () =>{
     const src = image.getAttribute("src");
     if(src == "/images/Furude_Rika_cat.png"){
-        image.setAttribute("src", "/images/meep.png")
+        image.setAttribute("src", "images/meep.png")
     } else{
-        image.setAttribute("src", "/images/Furude_Rika_cat.png");
+        image.setAttribute("src", "images/Furude_Rika_cat.png");
     }
 });
