9b47269fc9fddb390b5ab994354d5e0ba5b60f23
Steven Nguyen <stevennguyen223@gmail.com>
2026-03-05 22:34:51 -0500

    Now it's fixed

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const image = document.querySelector("img");
 
 image.addEventListener("click", () =>{
     const src = image.getAttribute("src");
-    if(src == "/images/Furude_Rika_cat.png"){
+    if(src == "images/Furude_Rika_cat.png"){
         image.setAttribute("src", "images/meep.png")
     } else{
         image.setAttribute("src", "images/Furude_Rika_cat.png");
